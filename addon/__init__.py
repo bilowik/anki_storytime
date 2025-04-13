f463f0876bebe9ed041536d12f4ce1e177a10bf9
bilowik <aaronjflores962@gmail.com>
2025-04-13 00:56:24 -0400

    Add some error handling; Print out response

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,15 +1,19 @@
 import sys
+import os
 
 # When bundlings the packages they will be placed here.
-sys.path.append("./libs")
+sys.path.insert(1, os.path.join(os.path.dirname((os.path.abspath(__file__))), 'libs'))
+print(sys.path)
 
-from typing import Callable, List, Union
+from typing import Callable, List, Union, Dict
 import aqt
 from aqt import mw
 import aqt.gui_hooks
 from aqt.main import AnkiQt
 from aqt.overview import Overview, OverviewContent
 from anki.collection import Collection
+from openai import OpenAI
+import openai
 
 AI_BUTTON_URI = "anki_storytime__ai_button";
 
@@ -17,6 +21,10 @@ DEFAULT_PROMPT_BASE = """Create a story in Japanese with the following vocabular
 
 """
 MAX_VOCAB_WORDS = 100
+    
+
+CONFIG: Dict = mw.addonManager.getConfig(__name__) or {}
+print(CONFIG)
 
 def main():
     aqt.gui_hooks.overview_will_render_bottom.append(add_ai_button)
@@ -37,12 +45,28 @@ def add_ai_button(link_handler: Callable[[str], bool], links: List[List[str]]) -
 
     links.append(['A', AI_BUTTON_URI, "AI Button"])
     def ai_button_link_handler(url: str):
+        handler = link_handler(url)
         if url == AI_BUTTON_URI:
             # TODO: Do something here?
             print(AI_BUTTON_URI + '_button_test')
             vocab: List[str]= get_forgotten_vocab(mw)
-            prompt: str = DEFAULT_PROMPT_BASE + "\n".join(vocab)
-        return link_handler(url)
+            if len(vocab) > 0:
+                api_key = CONFIG.get('OpenAI API Key', '')
+                if api_key:
+                    client = OpenAI(api_key=CONFIG.get('OpenAI API Key', ''))
+                    prompt: str = DEFAULT_PROMPT_BASE + "\n".join(vocab)
+                    try:
+                        response = client.responses.create(input=prompt, model="gpt-4o")
+                        print(response.output_text)
+                    except openai.RateLimitError:
+                        print("Out of credits")
+                    except openai.BadRequestError as e:
+                        print(f"Bad request: {e}")
+                else:
+                    print("No API Key provided")
+
+
+        return handler
 
     return ai_button_link_handler
 
