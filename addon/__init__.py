f4b4f6ac99b3f3ec3d753392211f196c4f79ec1b
bilowik <aaronjflores962@gmail.com>
2025-04-13 00:05:02 -0400

    Create AI prompt

--- addon/__init__.py
+++ addon/__init__.py
@@ -13,12 +13,16 @@ from anki.collection import Collection
 
 AI_BUTTON_URI = "anki_storytime__ai_button";
 
+DEFAULT_PROMPT_BASE = """Create a story in Japanese with the following vocabulary words that will be listed individually on separate lines starting after the first empty newline. The story can be about anything in particular, but should provide a reasonable amount of context, not every sentence has to have one of the vocabulary words in it, but you must utilize every single vocabulary word provided. The length of the story can be as long as needed to encompass all of the vocabulary words. Do not respond with any other text other than the story. Following are the vocabulary words:
+
+"""
+MAX_VOCAB_WORDS = 100
 
 def main():
     aqt.gui_hooks.overview_will_render_bottom.append(add_ai_button)
     return 
 
-def get_forgotten_cards(mw: AnkiQt) -> List[str]:
+def get_forgotten_vocab(mw: AnkiQt) -> List[str]:
     col: Union[Collection, None] = mw.col 
     if col is None:
         return []
@@ -36,7 +40,8 @@ def add_ai_button(link_handler: Callable[[str], bool], links: List[List[str]]) -
         if url == AI_BUTTON_URI:
             # TODO: Do something here?
             print(AI_BUTTON_URI + '_button_test')
-            print(get_forgotten_cards(mw))
+            vocab: List[str]= get_forgotten_vocab(mw)
+            prompt: str = DEFAULT_PROMPT_BASE + "\n".join(vocab)
         return link_handler(url)
 
     return ai_button_link_handler
