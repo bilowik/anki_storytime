7e95ff9c76058f7e1295184e583ceb643f48d5fd
bilowik <aaronjflores962@gmail.com>
2025-04-13 13:28:34 -0400

    Imrpove config typing

--- addon/__init__.py
+++ addon/__init__.py
@@ -4,7 +4,7 @@ import os
 # When bundlings the packages they will be placed here.
 sys.path.insert(1, os.path.join(os.path.dirname((os.path.abspath(__file__))), 'libs'))
 
-from typing import Callable, List, Union, Dict
+from typing import Callable, List, Union, Dict, TypedDict, cast, Set
 import aqt
 from aqt import mw
 import aqt.gui_hooks
@@ -15,10 +15,11 @@ from aqt.qt.qt6 import QDialog, QComboBox, QPushButton, QFormLayout, QLabel
 from anki.collection import Collection
 from openai import OpenAI
 import openai
-from typing import TypedDict
 
 AI_BUTTON_URI = "anki_storytime__ai_button";
 MAX_VOCAB_WORDS = 100
+
+
 
 class Prompt(TypedDict):
     name: str
@@ -31,6 +32,17 @@ class Theme(TypedDict):
 class VocabQuery(TypedDict):
     name: str
     query: str
+
+class Config(TypedDict):
+    openai_api_key: str
+    openai_model: str
+    MOCK_API_RESPONSE: str
+    vocab_query_presets: List[VocabQuery]
+    theme_presets: List[Theme]
+    prompt_presets: List[Prompt]
+    custom_vocab_query_presets: List[VocabQuery]
+    custom_theme_presets: List[Theme]
+    custom_prompt_presets: List[Prompt]
 
 class PromptForm(QDialog):
     def __init__(self, prompts: List[Prompt], vocab_queries: List[VocabQuery], themes: List[Theme], ):
@@ -88,8 +100,19 @@ def main():
     aqt.gui_hooks.overview_will_render_bottom.append(add_ai_button)
     return 
 
-def get_config() -> Dict:
-    return mw.addonManager.getConfig(__name__) or {}
+def get_config() -> Config:
+    config: Dict = mw.addonManager.getConfig(__name__) or {}
+    required_fields: Set[str] = set(Config.__required_keys__)
+    missing_fields: Set[str] = required_fields - set(config.keys())
+    
+    # Technically, the below will only ever occur due to a programming error since if the user
+    # tries to remove a field from the json config, it gets repopulated with the default value.
+    if missing_fields: 
+
+        showInfo(f"Anki Storytime addon is missing required configuration fields, this is likely a programming error. It may not function properly. Please try populating the provided fields or resetting to default values. Missing fields: {"\n".join(missing_fields)}")
+        raise Exception("Invalid configuration for Anki Storytime")
+    # Can safely cast here since we know it has all required fields.
+    return cast(Config, config)
 
 
 def get_vocab(mw: AnkiQt, query: str) -> List[str]:
@@ -105,7 +128,7 @@ def prepare_story_on_success(story: str) -> None:
 
 
 def prepare_story(vocab_query: str, theme: str, prompt: str) -> str:
-    config: Dict = get_config()
+    config: Config = get_config()
     vocab: List[str]= get_vocab(mw, vocab_query)
     if len(vocab) > 0:
         if config.get("MOCK_API_RESPONSE") is True:
@@ -131,7 +154,7 @@ def prepare_story(vocab_query: str, theme: str, prompt: str) -> str:
         raise Exception("No notes found matching your query")
 
 
-def create_prompt_dialog(config: Dict):
+def create_prompt_dialog(config: Config):
     vocab_queries: List[VocabQuery] = config["vocab_query_presets"]
     themes: List[Theme] = config["theme_presets"]
     prompts: List[Prompt] = config["prompt_presets"]
@@ -141,7 +164,7 @@ def create_prompt_dialog(config: Dict):
 
 
 def add_ai_button(link_handler: Callable[[str], bool], links: List[List[str]]) -> Callable[[str], bool]:
-    config: Dict = get_config()
+    config: Config = get_config()
     links.append(['A', AI_BUTTON_URI, "AI Button"])
     def ai_button_link_handler(url: str):
         handler = link_handler(url)
