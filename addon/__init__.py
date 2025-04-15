0303f7f2d8ee12afbb2dbb7adea98b44ec03ac3a
bilowik <bilowik@bilowik.com>
2025-04-15 17:10:01 -0400

    Remove sys path mangling

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# When bundlings the packages they will be placed here.
-sys.path.insert(1, os.path.join(os.path.dirname((os.path.abspath(__file__))), 'libs'))
-
 from typing import Callable, List, Union, Dict, TypedDict, cast, Callable, Set, Sequence 
 import urllib.request
 import json
